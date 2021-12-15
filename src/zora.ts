94bd5a60db8bee08c4194ff2e288594535fed071
Adam Gobran <adam.gobran@coinbase.com>
2021-12-14 22:14:29 -0500

    refactor zora mapping code

--- src/zora.ts
+++ src/zora.ts
@@ -1,42 +1,21 @@
-import { BigInt, log } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts"
 import { AuctionBid, AuctionEnded } from "../generated/AuctionHouse/AuctionHouse"
 import { ZoraBid } from "../generated/schema"
 import { ZORA_AUCTION_HOUSE_ADDRESS } from "./constants"
 import { _createSaleEntityFromParams } from "./shared"
 
 export function handleZoraBidCreated(event: AuctionBid): void {
-    let auctionId = event.params.auctionId.toString()
-    log.debug(`running handleZoraBidCreated for ${auctionId}`, [])
+    let activeBid = new ZoraBid(event.params.auctionId.toString())
 
-    let currentHighestBid = ZoraBid.load(_bidIdFromAuctionId(auctionId))
-
-    // if this is first bid for auction, create new bid entity
-    if (!currentHighestBid) {
-        let bid = new ZoraBid(_bidIdFromAuctionId(auctionId))
-        bid.amount = event.params.value
-        bid.bidder = event.params.sender
-        bid.save()
-        return
-    }
-
-    // otherwise, set bid as active bid
-    // if this event got emitted, this bid is the highest/active one
-    currentHighestBid.amount = event.params.value
-    currentHighestBid.bidder = event.params.sender
-    currentHighestBid.save()
+    activeBid.amount = event.params.value
+    activeBid.bidder = event.params.sender
+    activeBid.save()
 }
 
 export function handleZoraAuctionEnded(event: AuctionEnded): void {
-    let auctionId = event.params.auctionId.toString()
-    let buyer = event.params.winner
-    let seller = event.params.tokenOwner
-    let paymentToken = event.params.auctionCurrency
-    let nftContractAddress = event.params.tokenContract
-    let nftTokenId = event.params.tokenId.toString()
-
     // get price of sale from active bid entity, falling back to slightly incorrect event.params.amount if no bid was found (this should never happen though)
     let price: BigInt
-    let activeBid = ZoraBid.load(_bidIdFromAuctionId(auctionId))
+    let activeBid = ZoraBid.load(event.params.auctionId.toString())
     if (!activeBid) {
         price = event.params.amount
     } else {
@@ -44,9 +23,16 @@ export function handleZoraAuctionEnded(event: AuctionEnded): void {
     }
 
     let saleId = event.transaction.hash.toHexString()
-    _createSaleEntityFromParams(saleId, "Single", ZORA_AUCTION_HOUSE_ADDRESS, event.block, buyer, seller, paymentToken, price, nftContractAddress, nftTokenId)
-}
-
-function _bidIdFromAuctionId(auctionId: string): string {
-    return `${auctionId}-active-bid`
+    _createSaleEntityFromParams(
+        saleId,
+        "Single",
+        ZORA_AUCTION_HOUSE_ADDRESS,
+        event.block,
+        event.params.winner,
+        event.params.tokenOwner,
+        event.params.auctionCurrency,
+        price,
+        event.params.tokenContract,
+        event.params.tokenId.toString()
+    )
 }
